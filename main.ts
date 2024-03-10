5ff4d4de4812ecb4dc1d4cff097fd832253ed9a0
valgolei <84638017+valgolei@users.noreply.github.com>
2024-03-10 14:09:24 +0100

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,235 @@
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+}
+info.onScore(10, function () {
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    info.setScore(0)
+    info.changeLifeBy(1)
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -200
+    if (héros.isHittingTile(CollisionDirection.Bottom)) {
+        héros.vy = -200
     }
 })
-let mySprite: Sprite = null
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+    info.changeScoreBy(1)
+})
+function création_enemie () {
+    for (let valeur of tiles.getTilesByType(assets.tile`portail`)) {
+        enemie_normal = sprites.create(img`
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . 2 2 f f f f 2 2 . . . . 
+            . . . f f f f f f f f f f . . . 
+            . . f f f 5 f f f f 5 f f f . . 
+            . f f f f f f f f f f f f f f . 
+            . f f f f f f f f f f f f f f . 
+            8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+            . 8 f f f f f f f f f f f f 8 . 
+            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            8 f f f f f f f f f f f f f f 8 
+            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            8 f f f f f f f f f f f f f f 8 
+            . 8 5 5 5 5 5 5 5 5 5 5 5 5 8 . 
+            8 f f f f f f f f f f f f f f 8 
+            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+            f 8 f f f f f f f f f f f f 8 f 
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(enemie_normal, valeur)
+        tiles.setTileAt(valeur, assets.tile`transparency16`)
+        enemie_normal.ay = 500
+        if (randint(0, 1) == 1) {
+            enemie_normal.vx = -40
+        } else {
+            enemie_normal.vx = 40
+        }
+    }
+    for (let valeur of tiles.getTilesByType(assets.tile`myTile2`)) {
+        coin = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . 5 5 5 . . . 5 5 . . . . 
+            . . . . 5 5 . 5 5 5 5 5 . . . . 
+            . . . 5 5 5 . 5 5 5 5 5 5 . . . 
+            . . . 5 5 5 . 5 5 5 5 5 5 . . . 
+            . . . . 5 5 . 5 5 5 5 5 . . . . 
+            . . . . 5 5 5 . . . 5 5 . . . . 
+            . . . . . 5 5 5 5 5 5 . . . . . 
+            . . . . . . . 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.coin)
+        tiles.placeOnTile(coin, valeur)
+        tiles.setTileAt(valeur, assets.tile`transparency16`)
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (sprite.y < otherSprite.top) {
+        sprites.destroy(otherSprite)
+        héros.vy = -100
+        info.changeScoreBy(1)
+        music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+    } else {
+        if (info.life() == 1) {
+            sprites.destroy(héros)
+        }
+        music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.InBackground)
+        info.changeLifeBy(-1)
+        animation.runImageAnimation(
+        héros,
+        [img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f 2 2 2 2 2 2 f f f . . 
+            . . f f 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 f f f f f f 2 2 f . . 
+            . . f f f f 2 2 2 2 f f f f . . 
+            . f f 2 f 2 f 2 2 f 2 f 2 f f . 
+            . f 2 2 2 2 f 2 2 f 2 2 2 2 f . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . . f 2 2 2 2 2 2 2 2 f . . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f 2 2 2 2 2 2 f f f . . 
+            . . f f 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 f f f f f f 2 2 f . . 
+            . . f f f f 2 2 2 2 f f f f . . 
+            . f f 2 f 2 f 2 2 f 2 f 2 f f . 
+            . f 2 2 2 2 f 2 2 f 2 2 2 2 f . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . . f 2 2 2 2 2 2 2 2 f . . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f 2 2 2 2 2 2 f f f . . 
+            . . f f 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 f f f f f f 2 2 f . . 
+            . . f f f f 2 2 2 2 f f f f . . 
+            . f f 2 f 2 f 2 2 f 2 f 2 f f . 
+            . f 2 2 2 2 f 2 2 f 2 2 2 2 f . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . . f 2 2 2 2 2 2 2 2 f . . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f 2 2 2 2 2 2 f f f . . 
+            . . f f 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 f f f f f f 2 2 f . . 
+            . . f f f f 2 2 2 2 f f f f . . 
+            . f f 2 f 2 f 2 2 f 2 f 2 f f . 
+            . f 2 2 2 2 f 2 2 f 2 2 2 2 f . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . . f 2 2 2 2 2 2 2 2 f . . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `,img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `],
+        100,
+        false
+        )
+        pause(800)
+    }
+})
+let coin: Sprite = null
+let enemie_normal: Sprite = null
+let héros: Sprite = null
 scene.setBackgroundColor(9)
-mySprite = sprites.create(img`
+héros = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -24,15 +248,27 @@ mySprite = sprites.create(img`
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
 tiles.setCurrentTilemap(tilemap`niveau2`)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 8))
-scene.cameraFollowSprite(mySprite)
-controller.moveSprite(mySprite, 100, 0)
-mySprite.ay = 500
+tiles.placeOnTile(héros, tiles.getTileLocation(0, 8))
+scene.cameraFollowSprite(héros)
+controller.moveSprite(héros, 100, 0)
+héros.ay = 500
+info.setLife(3)
+création_enemie()
+game.onUpdate(function () {
+    for (let enemie_normal2 of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (enemie_normal2.isHittingTile(CollisionDirection.Left)) {
+            enemie_normal2.vx = randint(30, 50)
+        }
+        if (enemie_normal2.isHittingTile(CollisionDirection.Right)) {
+            enemie_normal2.vx = randint(-30, -50)
+        }
+    }
+})
 forever(function () {
-    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`myTile1`)) {
+    if (héros.tileKindAt(TileDirection.Center, assets.tile`myTile1`)) {
         game.gameOver(false)
     }
-    if (mySprite.tileKindAt(TileDirection.Center, sprites.dungeon.stairNorth)) {
+    if (héros.tileKindAt(TileDirection.Center, sprites.dungeon.stairNorth)) {
         game.gameOver(true)
     }
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -12,6 +12,8 @@ namespace myTiles {
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -35,7 +37,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "niveau2":
-            case "niveau2":return tiles.createTilemap(hex`1e000a00000000060000000000000000000000000000000000000000000000000000060000000000060006000006000000060000000600000006000000060000000006000600000000000000000600000000000000000000000600000000060000000000000000060000000000000006000006000000000000000000000000000600000000000000000000000000000000000001000600000600000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000001000001000000000000000000000100000000000100000000000001000001000001000000000000030303030103030303030103030301030301030301040401030303030305020202020202020202020202020202020202020202020202020202020202`, img`
+            case "niveau2":return tiles.createTilemap(hex`1e000a00000000060000000000000000000000000000000000000000000000000000060000000000060006000006000000060000000600080006000000060000000006000600000000000000000600000000000000000000000600000000060000000000000000060000000000000006080006000000000000080000000000000800080000000000080000000000000000000001000600000600000000000000000000000000000008000000000000000001000000000000000000000000000000000000000000000000000001000001000000000000000000000100000000070100000000000001000001000001070000000000030303030103030303030103030301030301030301040401030303030305020202020202020202020202020202020202020202020202020202020202`, img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -46,7 +48,7 @@ namespace myTiles {
 . . . . 2 . . . . . 2 . . . . . . 2 . . 2 . . 2 . . . . . . 
 . . . . 2 . . . . . 2 . . . 2 . . 2 . . 2 . . 2 . . . . . . 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,sprites.builtin.forestTiles24,sprites.swamp.swampTile0,myTiles.tile3,sprites.dungeon.stairNorth,myTiles.tile5,myTiles.tile4], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,sprites.builtin.forestTiles24,sprites.swamp.swampTile0,myTiles.tile3,sprites.dungeon.stairNorth,myTiles.tile5,myTiles.tile4,myTiles.tile6], TileScale.Sixteen);
         }
         return null;
     })
@@ -64,6 +66,8 @@ namespace myTiles {
             case "tile5":return tile5;
             case "portail":
             case "tile4":return tile4;
+            case "myTile2":
+            case "tile6":return tile6;
         }
         return null;
     })
